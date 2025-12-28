663fdca56a8efd512bc5d26780a41c31611651b5
FATIMAEZZAHRA-IZOUGARNE <fatimaezzahraizougarne@gmail.com>
2025-12-28 17:11:32 +0100

    Add files via upload

--- TestCollections.java
+++ TestCollections.java
@@ -1,36 +1,31 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class TestCollections {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		LinkedList<Etudiant> etudiants = new LinkedList<>();
-		
-		Etudiant e1 = new Etudiant("ray",001);
-		Etudiant e2 = new Etudiant("BOb",022);
-		Etudiant e3 = new Etudiant("Roubi",003);
-		Etudiant e4 = new Etudiant("Sara",9);
-		Etudiant e5 = new Etudiant("Alex",030);
-		
-		etudiants.addLast(e1);
-		etudiants.addLast(e2);
-		etudiants.addLast(e3);
-		etudiants.addLast(e4);
-		etudiants.addLast(e5);
-		
-		Set<Etudiant> etuds= new HashSet<>(etudiants);
-		
-		///-------------------- afficher linked list------------
-		System.out.println(etudiants);
-		///-------------------- afficher HashSet-----------
-		System.out.println(etuds);
-		
+    public static void main(String[] args) {
 
-	}
+        // TreeMap trié par matricule
+        Map<Integer, Etudiant> mapEtudiants = new TreeMap<>();
 
+        Etudiant e1 = new Etudiant("Ahmed", 12);
+        Etudiant e2 = new Etudiant("Sara", 5);
+        Etudiant e3 = new Etudiant("Yassine", 20);
+        Etudiant e4 = new Etudiant("Nour", 8);
+
+        // Ajout (clé = matricule)
+        mapEtudiants.put(e1.getMatricule(), e1);
+        mapEtudiants.put(e2.getMatricule(), e2);
+        mapEtudiants.put(e3.getMatricule(), e3);
+        mapEtudiants.put(e4.getMatricule(), e4);
+
+        // Parcours et affichage
+        System.out.println("Liste des étudiants triée par matricule :");
+        for (Map.Entry<Integer, Etudiant> entry : mapEtudiants.entrySet()) {
+            System.out.println(
+                "Matricule: " + entry.getKey() +
+                " | Etudiant: " + entry.getValue()
+            );
+        }
+    }
 }
