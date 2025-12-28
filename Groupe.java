3f19d08e9f770c3e7b713ec0259e85c0324a982d
FATIMAEZZAHRA-IZOUGARNE <fatimaezzahraizougarne@gmail.com>
2025-12-28 18:18:01 +0100

    Add methods for group average and student count
    Added methods to calculate group average and count students.

--- Groupe.java
+++ Groupe.java
@@ -26,5 +26,40 @@ public class Groupe {
     public void supprimerEtudiant(int matricule) {
         myMap.remove(matricule);
     }
+
+
+	/// moyenne générale du groupe
+    public double moyenneGroupe() {
+        if (myMap.isEmpty()) return 0;
+
+        double somme = 0;
+        for (Etudiant e : myMap.values()) {
+            somme += e.calculerM();
+        }
+        return somme / myMap.size();
+    }
+
+	//  nombre d'étudiants
+    public int nombreEtudiants() {
+        return myMap.size();
+    }
+    
+    // --------get le nom de groupe -----------------
+    public String getNom() {
+        return this.nomGroupe;
+    }
+    ///===========================================================
+    public static Etudiant rechercherDansTousLesGroupes(
+        Map<String, Groupe> groupes, int matricule) {
+
+        for (Groupe g : groupes.values()) {   // 1️- parcourir chaque groupe
+             Etudiant e = g.rechercherEtudiant(matricule); // 2️-chercher l'etudiant
+             if (e != null) {                  // 3-s'il existe
+                  return e;                     // 4️- on le retourne
+             }
+        }
+        return null;                          // 5️- non trouve
+    }
     
 }
+
