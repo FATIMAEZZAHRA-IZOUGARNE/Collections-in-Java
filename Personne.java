be3bdd9ce07a3848f3dcd6c46e970fe203b2019d
FATIMAEZZAHRA-IZOUGARNE <fatimaezzahraizougarne@gmail.com>
2025-12-28 01:09:04 +0100

    Add files via upload

---
+++ Personne.java
@@ -0,0 +1,39 @@
+import java.util.Objects;
+import java.util.Comparator;
+
+public class Personne implements Comparable<Personne> {
+	private String nom;
+	private int age;
+	
+	public Personne(String nom, int age) {
+		this.nom= nom;
+		this.age= age;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(nom,age);
+	}
+	
+	 @Override
+     public boolean equals(Object obj) {
+        if (this == obj)
+        	return true;
+	    if (obj instanceof Personne autre)
+	        return age == autre.age && nom.equals(autre.nom);
+	    return false;
+	 }
+	 
+	 //methode compareTo
+	 public int compareTo(Personne p) {
+		 return this.age- p.age;
+	 }
+	 
+	 //methode toString
+	 public String toString() {
+		 return "Nom :"+this.nom+" ||Age :"+this.age+"(ans)";
+	 }
+
+
+   
+}
